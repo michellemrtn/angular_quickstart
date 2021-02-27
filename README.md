270c8c38d24f8c4cf6fa70adcb755532d5eca1ae
Michelle Martin <MartinMi81419@th-nuernberg.de>
2021-02-27 16:09:45 +0100

    Upgrade to angular 11

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# CrmsFrontend
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.21.
+# BasicFrontend
 
 ## Development server
 
--- src/app/contacts/contacts-detail/contacts-detail.component.spec.ts
+++ src/app/contacts/contacts-detail/contacts-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ContactsDetailComponent } from './contacts-detail.component';
 
@@ -6,7 +6,7 @@ describe('ContactsDetailComponent', () => {
   let component: ContactsDetailComponent;
   let fixture: ComponentFixture<ContactsDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ContactsDetailComponent ]
     })
